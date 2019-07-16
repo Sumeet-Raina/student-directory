6fb32a9e169fc17e5f7e4d7c6d085104a4d32ea2
sumeet978 <searchsumeet@gmail.com>
2019-07-16 21:32:38 +0100

    Array list created

--- directory.rb
+++ directory.rb
@@ -1,15 +1,19 @@
-student_count = 11
+students = [
+  "Dr. Hannibal Lecter",
+  "Darth Vader",
+  "Nurse Ratched",
+  "Michael Coleone",
+  "Alex Delarge",
+  "The Wicked Witch of the West",
+  "Terminator",
+  "Freddy Krueger",
+  "The Joker",
+  "Joffery Baratheon",
+  "Norman Bates"
+]
 puts "The students of the Villains Academy"
 puts "_____________"
-puts "Dr. Hannibal Lecter"
-puts "Darth Vader"
-puts "Nurse Ratched"
-puts "Michael Coleone"
-puts "Alex Delarge"
-puts "The Wicked Witch of the West"
-puts "Terminator"
-puts "Freddy Krueger"
-puts "The Joker"
-puts "Joffery Baratheon"
-puts "Norman Bates"
-puts "Overall, we have #{student_count} great students"
+students.each do |student|
+  puts student
+end
+puts "Overall, we have #{students.count} great students "
