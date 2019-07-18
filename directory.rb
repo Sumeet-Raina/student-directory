3f50d6fd7184dd57525a1f691db3806195496918
sumeet978 <searchsumeet@gmail.com>
2019-07-18 15:37:01 +0100

    Exercise 7 sloution added

--- directory.rb
+++ directory.rb
@@ -1,55 +1,54 @@
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-  # create an empty array
   students = []
-  # get the first name
-  puts "Enter name:"
-  name = gets.chomp
-  puts "Enter country:"
-  country = gets.chomp
-  puts "Enter hobbies:"
-  hobbies = gets.chomp
-  puts "Enter height"
-  height = gets.chomp
-  # while the name is not empty, repeat this code
-  while !name.empty? do
-    # add the student hash to the array
-    students << {name: name, cohort: "November", country: country, hobbies: hobbies, height: height }
-    puts "Now we have #{students.count} students"
-    # get another name from the user
-    puts "Enter name"
-    name = gets.chomp
-    puts "Enter country"
-    country = gets.chomp
-    puts "Enter hobbies"
-    hobbies = gets.chomp
-    puts "Enter height"
-    height = gets.chomp
+  while name = gets.chomp.to_sym do
+    break if name.empty?
+    puts "What is your student cohort"
+    cohort = gets.chomp.to_sym
+    puts "What is your student height"
+    height = gets.chomp.to_sym
+    puts "What is your student hobbie"
+    hobbies = gets.chomp.to_sym
+    puts "Where is your student from"
+    country_of_birth = gets.chomp.to_sym
+    if cohort.empty? || height.empty? || hobbies.empty? || country_of_birth.empty?
+      cohort = "Not given"
+      height = "(Not given)"
+      hobbies = "(Not given)"
+      country_of_birth = "(Not given)"
+    end
+    students << {name: name, cohort: cohort, hobbies: hobbies, height: height, country_of_birth: country_of_birth}
+    if students.count > 1
+      puts "Now we have #{students.count} students"
+    else
+      puts "Now we have #{students.count} student"
+    end
+
   end
-  # return the array of students
   students
 end
 
 def print_header
-  puts "The students of Villains Academy"
-  puts "-------------"
+puts "The students of Villains Academy"
+puts "-------------"
 end
-def print(students)
-  i = 0
-while !(student = students[i]).nil? do
-   puts " #{student[:name].center(6)} #{student[:cohort].center(6)} cohort  country: #{student[:country].center(6)} hobbies: #{student[:hobbies].center(6)} height: #{student[:height].center(6)} "
-   i += 1
-end
-  end
 
+def print(students)
+  students.each do |student|
+     puts " #{student[:name]} (#{student[:cohort]} cohort), Country of birth: #{student[:country_of_birth]}  Height: #{student[:height]} cm Hobbies: #{student[:hobbies]}"
+  end
+end
 
 def print_footer(students)
-  puts "Overall, we have #{students.count} great students"
+  if students.count > 1
+    puts "Overall, we have #{students.count} great students"
+  else
+    puts "Overall, we have #{students.count} great student"
+  end
 end
 
 students = input_students
-#nothing happens until we call the methods
 print_header
 print(students)
 print_footer(students)
